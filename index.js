d5f85af98d0588ea9f7240b7887d3239cc111217
IPinkFreudI <machomancarl@gmail.com>
2025-01-03 03:46:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.post('/voice', (req, res) => {
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`Server running on port \${port}`);
+    console.log('Server running on port:', port);  // Changed this line
 });
